228cbe6859e891f94b644dddf97a49cf6574e82a
Zabrovskj <zabrovskj@gmail.com>
2014-01-11 22:13:18 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestAppRails4::Application.config.secret_key_base = 'cc04d3ac18d6f2387140ff975741032f7cebc951300b1058c5c3391505eb5ec5605031fb7245deb03734150aa8142ca2896aaa66aee585b307251d6b90067fef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestAppRails4::Application.config.session_store :cookie_store, key: '_TestAppRails4_session'
